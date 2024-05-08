d7e5da05fa5fa1718fb713ef8fe2ee3227242e66
artem <banderovaa@gmail.com>
2024-05-08 10:51:47 +0600

    _

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # osm2garmin-ubuntu
+## Requaried
+```bash
+sudo apt install mkgmap mkgmap-splitter wget
+```
+## Usage
+``` bash
+./garmin-map.sh URL
+```
+URL - something like: https://download.geofabrik.de/russia/kaliningrad-latest.osm.pbf
--- garmin-map.sh
+++ garmin-map.sh
@@ -1,27 +1,30 @@
 #! /bin/bash
 
-SCRIPT_PATH=$(dirname $0)
-cd $SCRIPT_PATH
+#Переменные можно изменять
+MAPNUMBER=64400055
+COUNTRY_NAME=RUSSIA
+COUNTRY_ABBR=RUS
 
-
-if [ -z $1  ]
-then
+if [ -z $1 ]; then
   echo 'Не указан URL с файлом карты'
-  echo 'Должно быть что-то вроде: https://download.geofabrik.de/russia/far-eastern-fed-district-latest.osm.pbf'  
+  echo 'Должно быть что-то вроде: https://download.geofabrik.de/russia/far-eastern-fed-district-latest.osm.pbf'
   exit 1
 fi
 
+SCRIPT_PATH=$(dirname $0)
+cd $SCRIPT_PATH
+
 URL=$1
 FILENAME=$(basename $URL)
-MAPNAME=$(basename -s .osm.pbf $URL )
+MAPNAME=$(basename -s .osm.pbf $URL)
 
 wget $URL
 mkgmap-splitter --keep-complete=true $FILENAME
 rm $FILENAME
 
-mkgmap --gmapsupp -c template.args -n 64400055 --description=$MAPNAME --country-name=RUSSIA --country-abbr=RUS --unicode --lower-case --index --split-name-index --remove-ovm-work-files=true 
+mkgmap --gmapsupp -c template.args -n $MAPNUMBER --description=$MAPNAME --country-name=$COUNTRY_NAME --country-abbr=$COUNTRY_ABBR --unicode --lower-case --index --split-name-index --remove-ovm-work-files=true
 
 rm -f *.osm.pbf
 rm -f areas.list areas.poly densities-out.txt template.args osmmap.tdb osmmap.img
 rm -f 6324*.img
-mv gmapsupp.img $($MAPNAME).img
+mv gmapsupp.img $MAPNAME.img
